652099853ae19db8897e103b84915370d017eff4
Jason Efthimiou <Jason Efthimiou>
2017-03-17 13:35:17 -0400

    Initial Upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        
+        // System.out.println("");
+        // System.out.println("");
+
+        // Test to see if we can all access the project, this will be
+        // very helpful if we want to get this thing done.
+
+        // Uncomment one of the above and put your name as the string
+    }
+}
